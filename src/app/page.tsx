caf6d653eef89e04f00d74344faff2d119ca0df5
Krishna Sahu <dvlprkrishna@gmail.com>
2024-05-21 13:38:45 +0530

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Container from "@/components/Container";
 import Footer from "@/components/Footer";
 import HeroIntro from "@/components/HeroIntro";
 import Subscribe from "@/components/Subscribe";
@@ -8,12 +9,12 @@ export default function Home() {
   return (
     <>
       <HeroIntro />
-      <div className="header-container mt-8">
+      <Container className="header-container mt-8">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, tempore
         voluptas. Veritatis, doloremque obcaecati. Quae totam, laborum vel quo
         blanditiis consectetur perspiciatis deleniti obcaecati vitae quisquam
         assumenda ratione inventore autem.
-      </div>
+      </Container>
       <div className="mt-8 flex-col px-10 ">
         <Subscribe />
       </div>
