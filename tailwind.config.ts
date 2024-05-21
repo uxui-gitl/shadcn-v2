757d8650a27f4bf56ddd7ed32922e71ed871dcf0
Krishna Sahu <dvlprkrishna@gmail.com>
2024-05-21 12:48:14 +0530

    tw config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,17 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -18,6 +22,107 @@ const config = {
       },
     },
     extend: {
+      fontSize: {
+        "10xl": "10rem",
+      },
+
+      letterSpacing: {
+        tightest: "-.06em",
+      },
+      gradientColorStops: {
+        "gradient-1-start": "#007CF0",
+        "gradient-1-end": "#00DFD8",
+        "gradient-2-start": "#7928CA",
+        "gradient-2-end": "#FF0080",
+        "gradient-3-start": "#FF4D4D",
+        "gradient-3-end": "#F9CB28",
+      },
+      keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+        "slide-left": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+        "gradient-foreground-1": {
+          "from, 16.667%, to": {
+            opacity: "1",
+          },
+          "33.333%, 83.333%": {
+            opacity: "0",
+          },
+        },
+        "gradient-background-1": {
+          "from, 16.667%, to": {
+            opacity: "0",
+          },
+          "25%, 91.667%": {
+            opacity: "1",
+          },
+        },
+        "gradient-foreground-2": {
+          "from, to": {
+            opacity: "0",
+          },
+          "33.333%, 50%": {
+            opacity: "1",
+          },
+          "16.667%, 66.667%": {
+            opacity: "0",
+          },
+        },
+        "gradient-background-2": {
+          "from, to": {
+            opacity: "1",
+          },
+          "33.333%, 50%": {
+            opacity: "0",
+          },
+          "25%, 58.333%": {
+            opacity: "1",
+          },
+        },
+        "gradient-foreground-3": {
+          "from, 50%, to": {
+            opacity: "0",
+          },
+          "66.667%, 83.333%": {
+            opacity: "1",
+          },
+        },
+        "gradient-background-3": {
+          "from, 58.333%, 91.667%, to": {
+            opacity: "1",
+          },
+          "66.667%, 83.333%": {
+            opacity: "0",
+          },
+        },
+      },
+      animation: {
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+        "slide-left-infinite": "slide-left 15s linear infinite",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "gradient-background-1": "gradient-background-1 8s infinite",
+        "gradient-foreground-1": "gradient-foreground-1 8s infinite",
+        "gradient-background-2": "gradient-background-2 8s infinite",
+        "gradient-foreground-2": "gradient-foreground-2 8s infinite",
+        "gradient-background-3": "gradient-background-3 8s infinite",
+        "gradient-foreground-3": "gradient-foreground-3 8s infinite",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -58,23 +163,20 @@ const config = {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
+
+export default config;
