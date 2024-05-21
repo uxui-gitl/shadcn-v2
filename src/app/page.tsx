4657f3de8556048e23d6c5161c23fb6e384c743f
Krishna Sahu <dvlprkrishna@gmail.com>
2024-05-22 01:37:15 +0530

    hub of exp.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Container from "@/components/Container";
 import DesktopNavbar from "@/components/DesktopNavbar";
 import Footer from "@/components/Footer";
 import HeroIntro from "@/components/HeroIntro";
+import HubExpertise from "@/components/HubExpertise";
 import Robot from "@/components/Robot";
 import Subscribe from "@/components/Subscribe";
 import Image from "next/image";
@@ -14,6 +15,8 @@ export default function Home() {
       <HeroIntro />
       <ClientLogos />
       <Robot />
+      <HubExpertise />
+
       <Container className="header-container mt-8">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, tempore
         voluptas. Veritatis, doloremque obcaecati. Quae totam, laborum vel quo
