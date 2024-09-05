51289e1061677d31cdd01d275fbf021b1d7f1303
godvabhay <vabhay@godrej.com>
2024-09-05 10:30:02 +0530

    transfermation

---
+++ src/app/solutions/(transfermation)/customer-experience/microsoft-crm/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+
+const page = () => {
+
+  
+  return (
+    <h1>microsift-crm</h1>
+  );
+};
+
+export default page;
---
+++ src/app/solutions/(transfermation)/enterprise-suite/infor/infor-cloudsuite/page.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+
+const page = () => {
+  return (
+    <h1>infor cloudsuit</h1>
+  );
+};
+
+export default page;
