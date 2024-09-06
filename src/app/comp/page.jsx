1606da5d8fddc4bc56ab2cb7080448cd5f741bdc
godvabhay <vabhay@godrej.com>
2024-09-06 16:47:28 +0530

    minor fix

--- src/app/comp/page.jsx
+++ src/app/comp/page.jsx
@@ -1,49 +1,11 @@
-"use client";
 
-import { useState } from "react";
-import { useScroll } from "framer-motion";
-import { projects } from "../../assets/data";
-import { useEffect, useRef } from "react";
-import Lenis from "@studio-freight/lenis";
 export default function Home() {
-  const container = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: container,
-    offset: ["start start", "end end"],
-  });
 
-  useEffect(() => {
-    const lenis = new Lenis();
 
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-  });
   return (
     <>
-      <>
         {/* Intro Section */}
-
-
-        <>
-          <div className=" max-w-screen-xl  mb-5 mx-auto p-5   px-[2rem]">
-            <main ref={container} className={`relative mt-[20vh]`}>
-              {projects.map((project, i) => {
-                const targetScale = 1 - (projects.length - i) * 0.05;
-                return (
-                  // console.log(targetScale),
-                 
-                );
-              })}
-            </main>
-          </div>
         </>
-
-       
-      </>
-    </>
+    
   );
 }
